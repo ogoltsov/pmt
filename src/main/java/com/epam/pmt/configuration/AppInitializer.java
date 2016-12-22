12598de31f9d50d3d222cc66ca900100a8c21d51
Kirill_Ogoltsov <k.ogoltsov@gmail.com>
2016-12-22 13:22:16 +0600

    Migrate Spring setting in java code from XML; Migrate Hibernate settings to java code from XML; Add application properties from Hibernate settings;

---
+++ src/main/java/com/epam/pmt/configuration/AppInitializer.java
@@ -0,0 +1,28 @@
+package com.epam.pmt.configuration;
+
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { AppConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+}
